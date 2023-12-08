eca511a1561b80b5b24ef3d65d47e54e8b3a471c
ThureinS <94038134+ThureinS@users.noreply.github.com>
2023-12-08 23:09:52 +0630

    Complete mi card flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,91 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.white,
+              foregroundImage: AssetImage("images/bgRemoved.png"),
+            ),
+            Text(
+              "Thurein Soe",
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontWeight: FontWeight.bold,
+                fontSize: 40.0,
+              ),
+            ),
+            Text(
+              'Flutter Developer'.toUpperCase(),
+              style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 30.0,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5),
+            ),
+            SizedBox(
+              width: 250.0,
+              height: 20.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+                thickness: 3.0,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+959 123 456 789',
+                  style: TextStyle(
+                    color: Colors.teal,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'SourceSansPro',
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.mail,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'abcd@gmail.com',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal,
+                    fontFamily: 'SourceSansPro',
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+            )
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
